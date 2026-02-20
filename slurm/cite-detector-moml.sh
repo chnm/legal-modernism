9d8b7ec3e6c27cdde19fc50d1137b2bf669d96c9
Lincoln Mullen <lincoln@lincolnmullen.com>
2026-02-20 14:45:49 -0500

    Adjust slurm settings for correct number of workers
    Closes #116

--- slurm/cite-detector-moml.sh
+++ slurm/cite-detector-moml.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Run the diagnostic script with some allocations for testing
+# Run the citation detector over MOML treatises
 
-#SBATCH --job-name=lm-diagnostic   
+#SBATCH --job-name=cite-detector-moml
 #SBATCH --output=/scratch/%u/logs/%j-%x-%N.out   
 #SBATCH --error=/scratch/%u/logs/%j-%x-%N.log   
 #SBATCH --nodes=1  
 #SBATCH --ntasks=1  
-#SBATCH --cpus-per-task=10  
-#SBATCH --time=0-01:00:00  
-#SBATCH --mem=10GB  
-#SBATCH --partition normal  
+#SBATCH --cpus-per-task=128
+#SBATCH --time=5-00:00:00  
+#SBATCH --mem=512GB  
+#SBATCH --partition bigmem  
 #SBATCH --mail-user lmullen@gmu.edu  
 #SBATCH --mail-type BEGIN  
 #SBATCH --mail-type END  
 #SBATCH --mail-type FAIL  
 
 ## Run the program 
-~/legal-modernism/bin/lm-diagnostic
+~/legal-modernism/bin/cite-detector-moml
